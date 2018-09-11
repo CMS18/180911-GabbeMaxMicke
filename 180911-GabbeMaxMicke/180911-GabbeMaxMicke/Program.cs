65136b6d95a6953629078c3fe9759f6cf620e689
michaelfors <michaelfors@MICHAELFORS723D>
2018-09-11 14:04:22 +0200

    ändrat igen å igen

--- 180911-GabbeMaxMicke/180911-GabbeMaxMicke/Program.cs
+++ 180911-GabbeMaxMicke/180911-GabbeMaxMicke/Program.cs
@@ -48,7 +48,7 @@ namespace _180911_GabbeMaxMicke
             string input = Console.ReadLine();
             Console.WriteLine("Härligt! Att\""+input+"\" låter som en bra idé");
             Console.WriteLine("Vad har du under skon?: ");
-            string underSkoInput = Console.ReadLine();
+            underSkoInput = Console.ReadLine();
 
             Console.WriteLine("Lät ju mindre härligt...");
             Console.WriteLine("Är det okej om jag berättar min livshistoria?");
