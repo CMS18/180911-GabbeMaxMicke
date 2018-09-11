c72777c0eced2bb0f08202d32ca664f025cb3f60
Maximilian <max-dz@hotmail.com>
2018-09-11 13:58:43 +0200

    lagt till frågor. Korrat lite stavfel från Gabbe.

--- 180911-GabbeMaxMicke/180911-GabbeMaxMicke/Program.cs
+++ 180911-GabbeMaxMicke/180911-GabbeMaxMicke/Program.cs
@@ -13,7 +13,7 @@ namespace _180911_GabbeMaxMicke
             Console.WriteLine("Mata in namn: ");
             string userName = Console.ReadLine();
             Console.WriteLine("Trevligt att träffas " + userName + " jag är den hemliga mästarkodaren.");
-            string saga = ""+ userName + " har varit på ICA och handlat smör och ";
+           
 
             string saga = ""+ userName + " har varit på ICA och handlat smör och pinnar";
 
@@ -32,11 +32,12 @@ namespace _180911_GabbeMaxMicke
 
             Console.WriteLine(userLife + " hmmm...");
             Console.WriteLine("Could you be the coder I've been waiting for?");
+            Console.WriteLine("Nej, tyvärr.");
 
-            Console.WriteLine("Jaha! Så du har spenderat livet i {0} fan vad tråkigt!", userLife);
-            Console.WriteLine("Du har inte funderat på att prova något annat?");
+            Console.WriteLine("Jaha! Så du har spenderat livet i {0}, fan vad tråkigt!", userLife);
+            Console.WriteLine("Du har inte funderat på att prova något annat? I sådant fall, vad?");
             string userInputProv = Console.ReadLine();
-
+            Console.WriteLine("Fast {0} är ett riktigt kasst yrke. Tänk om!", userInputProv);
             
             Console.WriteLine("Vad har du under skon?: ");
             string underSkoInput = Console.ReadLine();
