0e8d349b61fb797f707b0ccfc295f19f65e4fa2a
michaelfors <michaelfors@MICHAELFORS723D>
2018-09-11 13:58:18 +0200

    smör och pinnar

--- 180911-GabbeMaxMicke/180911-GabbeMaxMicke/Program.cs
+++ 180911-GabbeMaxMicke/180911-GabbeMaxMicke/Program.cs
@@ -13,9 +13,8 @@ namespace _180911_GabbeMaxMicke
             Console.WriteLine("Mata in namn: ");
             string userName = Console.ReadLine();
             Console.WriteLine("Trevligt att träffas " + userName + " jag är den hemliga mästarkodaren.");
-            string saga = ""+ userName + " har varit på ICA och handlat smör och ";
 
-            string saga = ""+ userName + " har varit på ICA och handlat smör och pinnar";
+            string saga = ""+ userName + " har varit på ICA och handlat smör och pinnar!";
 
             Console.WriteLine("Vad brukar du handla?");
             string extraMatvara = Console.ReadLine();
