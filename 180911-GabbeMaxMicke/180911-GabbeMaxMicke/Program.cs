90cdd2fae46cff6261af67b925d1ec8b03a36915
Maximilian <max-dz@hotmail.com>
2018-09-11 13:34:57 +0200

    Step 1, DONE

---
+++ 180911-GabbeMaxMicke/180911-GabbeMaxMicke/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180911_GabbeMaxMicke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in namn: ");
+            string userName = Console.ReadLine();
+        }
+    }
+}
